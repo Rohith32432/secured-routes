b357e78c8dd25c8a204dd248e0fc9b3e45bdfc7b
Rohith_k <2100032432cse@gmail.com>
2024-01-05 21:32:39 +0530

    jsut

--- display/src/App.js
+++ display/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>SECURED ROLE BASED AUTENTICATION</h1>
+     <Navbar/>
     </div>
   );
 }
