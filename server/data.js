1737469eb8e5ae5211e688b67776dfe793c43f80
Rohith_k <2100032432cse@gmail.com>
2024-01-11 21:22:59 +0530

    checking

--- server/data.js
+++ server/data.js
@@ -1,48 +1,55 @@
-{
+ const users= 
  [
       {
-        "name": "John Admin",
-        "email": "john.admin@example.com",
+        "name": "admin",
+        "email": "admin@gmail.com",
         "role": "Admin",
+        "password":123,
         "permissions": "Full access to all features"
       },
       {
         "name": "Jane Admin",
         "email": "jane.admin@example.com",
         "role": "Admin",
+        "password":123,
         "permissions": "Full access to all features"
       },
       {
         "name": "Mike Admin",
         "email": "mike.admin@example.com",
         "role": "Admin",
+        "password":123,
         "permissions": "Full access to all features"
       },
       {
         "name": "David Developer",
         "email": "david.developer@example.com",
         "role": "Developer",
+        "password":123,
         "programming_language": "JavaScript",
         "permissions": "Access to code repositories and development tools"
       },
       {
-        "name": "Emma Developer",
-        "email": "emma.developer@example.com",
+        "name": "Rohith_K",
+        "email": "2100032432cseh@gmail.com",
         "role": "Developer",
-        "programming_language": "Python",
+        "password":123,
+        "programming_language": "javaScript",
         "permissions": "Access to code repositories and development tools"
       },
       {
         "name": "Charlie Developer",
         "email": "charlie.developer@example.com",
         "role": "Developer",
+        "password":123,
         "programming_language": "Java",
         "permissions": "Access to code repositories and development tools"
       },
       {
-        "name": "Alice Client",
-        "email": "alice.client@example.com",
+        "name": "RVS",
+        "email": "abc@gmail.com",
         "role": "Client",
+        "password":123,
         "company": "ABC Corp",
         "permissions": "Read-only access to project status"
       },
@@ -50,6 +57,7 @@
         "name": "Bob Client",
         "email": "bob.client@example.com",
         "role": "Client",
+        "password":123,
         "company": "XYZ Inc",
         "permissions": "Read-only access to project documents"
       },
@@ -57,9 +65,10 @@
         "name": "Eva Client",
         "email": "eva.client@example.com",
         "role": "Client",
+        "password":123,
         "company": "LMN Ltd",
         "permissions": "Read-only access to support tickets"
       }
     ]
-  }
   
+  module.exports=users
--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const moongose=require('mongoose')
 const {users, ROLE}=require('./models/data')
 const {checklogin,roleauth}=require('./basicauth.js')
 const view = require('./middleware/view.js')
+const data=require('./data.js')
 server.use(express.json())
-server.use(setUser)
+// server.use(setUser)
 server.use('/view', view)
 // main().catch(err => console.log(err));
      
@@ -27,16 +28,24 @@ server.get('/admin',checklogin,roleauth(ROLE.ADMIN),(req,res)=>{
 server.get('/board',checklogin,(req,res)=>{
   res.send('dashboard page')
 })
+server.get('/validate', (req, res) => {
+  const pwd=req.body.pwd
+  const email=req.body.email
+  const chk=data.find(user=>user.email===email && user.password==pwd)
+  res.json(chk);
+});
 
 
-function setUser(req, res, next) {
-  const userId = req.body.userid
-  console.log(userId);
-  if (userId) {
-    req.user = users.find(user => user.id === userId)
-  }
-  next()
-}
+
+
+// function setUser(req, res, next) {
+//   const userId = req.body.userid
+//   console.log(userId);
+//   if (userId) {
+//     req.user = users.find(user => user.id === userId)
+//   }
+//   next()
+// }
 
 server.listen(32432,()=>{
     console.log("server started at 32432");
