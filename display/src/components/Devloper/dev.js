27450549f6f09b1faafe1112d5c21f24296ef5a5
Rohith_k <2100032432cse@gmail.com>
2024-02-29 22:05:23 +0530

    testing ✔️

--- display/src/components/Devloper/dev.js
+++ display/src/components/Devloper/dev.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function Developr() {
+  const [data, setData] = useState([]);
+  const [poster, setPoster] = useState({});
+  const [count, setCount] = useState(1);
 
-  const [data,setdata]=useState([])
-  async function getdata(){
-  const data=await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_APIKEY}`)
-  const res=await data.json() 
- setdata(res.results)
-}
-getdata()
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${count}&sort_by=popularity.desc&api_key=${process.env.REACT_APP_APIKEY}`);
+      const result = await response.json();
+      setData(result.results);
+      if (result.results.length > 0) {
+        setPoster(result.results[0]);
+      }
+    }
+    fetchData();
+  }, [count]); 
 
+  const handleHover = (i) => {
+    setPoster(data[i]);
+  };
+console.log(poster);
   return (
     <div>
-      {
-       
-        data.map((e,i)=>([
-          <div key={i}>
-          
-            <img src={`https://image.tmdb.org/t/p/w500/${e.backdrop_path}`} alt="sj" />
-            <h2>{e.original_title}</h2>
-            <img src={`https://image.tmdb.org/t/p/w500/${e.poster_path}`} alt="sj" />
-          </div>
-       ] ))
-      }
+      <div className="top">
+        <h1>{poster.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/w500/${poster.backdrop_path}`} alt="" />
+        <h3>{poster.release_date}</h3>
+        <h3>{poster.vote_average}</h3>
+      </div>
+
+      <button onClick={() => setCount(count + 1)}>click</button>
+    <div style={{display:'flex',overflowX:'scroll'}}>
+      {data.map((e, i) => (
+        <div key={i} onMouseOver={() => handleHover(i)}>
+          <img src={`https://image.tmdb.org/t/p/w500/${e.backdrop_path}`} alt="sj" />
+          <h2>{e.original_title}</h2>
+        </div>
+      ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Developr
+export default Developr;
